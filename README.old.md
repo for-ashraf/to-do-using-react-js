0200cbfbc2ea685d9b4ce07d1c73a91de128ed7a
for-ashraf <46405183+for-ashraf@users.noreply.github.com>
2022-08-09 00:10:40 +0500

    Files are added
    Incorporated all the requirements

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# to-do-using-react-js
+This app is a React todo app which allows a user to add, edit and remove a task. The app is intended to consolidate an understanding of React, components, props, state, and hooks, by following a tutorial.
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TodoContainer from './functionBased/components/TodoContainer';
+
+// stylesheet
+import './functionBased/App.css';
+
+ReactDOM.render(<TodoContainer />, document.getElementById('root'));
